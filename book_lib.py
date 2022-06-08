cedb239f2f5457c69c6c18748209745b59fe26ab
inAsees <gurasees.singh121@gmail.com>
2022-06-08 10:20:32 +0530

    new API created for finding books in price range.

--- book_lib.py
+++ book_lib.py
@@ -1,3 +1,4 @@
+import bson.json_util as json_util
 import pymongo
 from flask import Flask, request
 
@@ -14,10 +15,30 @@ def get_book():
     res = []
     for book in books_obj:
         if text["name"] in book["book_name"]:
-            res.append(book["book_name"])
+            res.append(json_util.dumps(book))
     if len(res) == 0:
-        return {"status": "No book found"}, 400
-    return {"books": res}, 200
+        return {"status": "No books found"}, 400
+    elif res:
+        return {"books": res}, 200
+    else:
+        return {"status": "Some ERROR occurred."}, 400
 
 
-app.run(debug=True)
+@app.route('/api/books_in_price_range', methods=['POST'])
+def get_books_in_price_range():
+    text = request.json
+    books_obj = mydb["BOOKS"].find()
+    res = []
+    for book in books_obj:
+        if text["min_price"] <= book["rent_per_day"] <= text["max_price"]:
+            res.append(json_util.dumps(book))
+    if len(res) == 0:
+        return {"status": "No books found"}, 400
+    elif res:
+        return {"books": res}, 200
+    else:
+        return {"status": "Some ERROR occurred."}, 400
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
