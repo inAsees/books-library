9d0269f45bd9739ce6304211ca45e00976641d28
inAsees <gurasees.singh121@gmail.com>
2022-06-09 00:12:02 +0530

    method book_issue deleted.

--- insert_sample_dataset.py
+++ insert_sample_dataset.py
@@ -1,5 +1,3 @@
-from datetime import datetime
-
 import pymongo
 
 
@@ -19,39 +17,8 @@ def insert_sample_dataset():
         books.insert_one({"book_name": name, "category": category, "rent_per_day": rent})
 
 
-def book_issue():
-    transactions = mydb["TRANSACTIONS"]
-    persons = ["ram", "shyam", "geeta", "sidhu", "iyyer", "gopal", "ruby", "lalita", "sargun", "chotu"]
-    books_name = ["In Search of Lost Time", " Ulysses ", "Don Quixote", "The Great Gatsby", "Moby Dick", "Hamlet",
-                  "The Odyssey", "The Iliad", "One Hundred Years of Solitude", "The Republic", "The Book of Job",
-                  "The Book of Kahlil", "War and Peace ", "Madame Bovary", "The Divine Comedy",
-                  "The Brothers Karamazov", "Crime and Punishment", "The Metamorphosis", "The Brothers Grimm",
-                  "The Picture of Dorian Gray"]
-    issue_dates = ["2022-01-1", "2022-01-2", "2022-01-3", "2022-01-4", "2022-01-5", "2022-01-6", "2022-01-7",
-                   "2022-01-8", "2022-01-9", "2022-01-10"]
-    return_dates = ["2022-01-11", "2022-01-12", "2022-01-13", "2022-01-14", "2022-01-15", "2022-01-16", "2022-01-17",
-                    "2022-01-18", "2022-01-19", "2022-01-20"]
-
-    for person, book, issue_date, return_date in zip(persons, books_name, issue_dates, return_dates):
-        try:
-            yr, mon, day = map(int, issue_date.split('-'))
-            issue_date = datetime(yr, mon, day)
-        except ValueError:
-            print("Invalid issue date:", issue_date)
-
-        try:
-            yr, mon, day = map(int, return_date.split('-'))
-            return_date = datetime(yr, mon, day)
-        except ValueError:
-            print("Invalid return date:", return_date)
-
-        transactions.insert_one(
-            {"person_name": person, "book_name": book, "issue_date": issue_date, "return_date": return_date})
-
-
 if __name__ == "__main__":
     myclient = pymongo.MongoClient("mongodb://localhost:27017")
     mydb = myclient["library"]
 
     insert_sample_dataset()
-    book_issue()
