487a793eeed808fe36fcf1ece333e1833b4faf88
inAsees <gurasees.singh121@gmail.com>
2022-06-08 10:33:04 +0530

    new API '/api/books_in_pr_range_name_and_category' created .

--- book_lib.py
+++ book_lib.py
@@ -40,5 +40,22 @@ def get_books_in_price_range():
         return {"status": "Some ERROR occurred."}, 400
 
 
+@app.route('/api/books_in_pr_range_name_and_category', methods=['POST'])
+def get_books_in_price_range_name_and_category():
+    text = request.json
+    books_obj = mydb["BOOKS"].find()
+    res = []
+    for book in books_obj:
+        if text["min_price"] <= book["rent_per_day"] <= text["max_price"] and text["name"] in book["book_name"] and \
+                text["category"] in book["category"]:
+            res.append(json_util.dumps(book))
+    if len(res) == 0:
+        return {"status": "No books found"}, 400
+    elif res:
+        return {"books": res}, 200
+    else:
+        return {"status": "Some ERROR occurred."}, 400
+
+
 if __name__ == '__main__':
     app.run(debug=True)
